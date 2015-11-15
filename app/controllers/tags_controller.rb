0e56698b8ef829b5d72d1921db987986ce12eb2a
Chase Gruber <chasegruber@gmail.com>
2015-11-15 10:39:06 -0500

    configure tags controller, create new tag associated with user

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,8 +2,15 @@ class TagsController < ApplicationController
 
   def create
     @event = Event.find(params[:event_id])
-    @tag = Tag.create(tag_params)
-    @event.tags << @tag
+    @tag = Tag.find_or_create_by(tag_params)
+    existing_tagging = Tagging.find_by(event: @event, user:current_user.id, tag:@tag)
+    unless existing_tagging
+      Tagging.create(event: @event, user:current_user, tag:@tag)
+    end
+    # tag = tag_params
+    # tag[:user] => current_user
+    # @tag = Tag.create(tag)
+    # @event.tags << @tag
     redirect_to @event
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
-
 end

e3ad169539160535dcb923e8437b8deac9f1f704
Chase Gruber <chasegruber@gmail.com>
2015-11-16 12:17:39 -0500

    users can follow and unfollow on a button click

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,16 @@ class UsersController < ApplicationController
 #
   def follow
     @followed = User.find(params[:id])
-    Following.create(follower:current_user,followed:@followed)
+    existing_follow = Following.find_by(follower: current_user, followed:@followed)
+    unless existing_follow
+      Following.create(follower:current_user,followed:@followed)
+    end
+    redirect_to event_path(session[:event_id])
+  end
+
+  def unfollow
+    @followed = User.find(params[:id])
+    Following.find_by(follower:current_user,followed:@followed).destroy
     redirect_to event_path(session[:event_id])
   end
 

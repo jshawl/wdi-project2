7c77007153ddcdd8d63876264c556b4ff4acbbc8
Chase Gruber <chasegruber@gmail.com>
2015-11-20 07:50:32 -0500

    done working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,14 +37,6 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     session[:event_id] = params[:id]
     @attendees = @event.users
-    @breakdown = {
-      :mf => @attendees.where(gender:'m',preference:'f').length,
-      :fm => @attendees.where(gender:'f',preference:'m').length,
-      :mm => @attendees.where(gender:'m',preference:'m').length,
-      :ff => @attendees.where(gender:'f',preference:'f').length,
-      :mb => @attendees.where(gender:'m',preference:'b').length,
-      :fb => @attendees.where(gender:'f',preference:'b').length
-    }
     uniq_tags = @event.tags.uniq{ |t| t }
     @tags = uniq_tags.map{|tg|{tag:tg,count:Tagging.where(event:@event,tag:tg).length}}
     @events = Event.all.where("created_at >= ?",Time.now.beginning_of_day.in_time_zone("UTC"))

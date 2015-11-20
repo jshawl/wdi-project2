037e776027b68d464070f78d85bea61802774f9b
Chase Gruber <chasegruber@gmail.com>
2015-11-19 20:08:15 -0500

    add past event to map if none exist for a given day, prevents map from erroring out

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,11 @@ class EventsController < ApplicationController
     @events = Event.all.where("created_at >= ?",Time.now.beginning_of_day.in_time_zone("UTC"))
       .where("created_at < ?", Time.now.in_time_zone("UTC"))
       .order(when: :desc)
+
+    ## so that there's something to prevent the map from erroring out
+    unless @events
+      @events = Event.last(2)
+    end
     att = @events.map{|e| e.users.size}
     # vote = @events.map{|e| e.get_upvotes.size - e.get_downvotes.size}
     # t = @events.map{|e| e.votes_for.size}
